509adfdc8d82d77a8ab16e21ef51029cba525f62
anCoderr <deyol.anurag999@gmail.com>
2022-08-21 19:47:15 +0530

    seg bit tree changed

--- DataStructures_and_Algorithms_in_PYTHON/Tree/binary_indexed_tree.py
+++ DataStructures_and_Algorithms_in_PYTHON/Tree/binary_indexed_tree.py
@@ -1,37 +1,51 @@
-class FenwickTree:
-    def __init__(self, n, data):
-        self.n = n
-        self.fenwick_tree = [0 for _ in range(0, n + 1)]
-        self.construct(data)
+class BinaryIndexedTree:
 
-    def construct(self, data):
-        for i in range(self.n):
-            self.update(i, data[i])
+    @staticmethod
+    def get_next_index(index) -> int:
+        return index + (index & -index)
 
-    def update(self, index, val):
+    @staticmethod
+    def get_parent_index(index) -> int:
+        return index - (index & -index)
+
+    def __init__(self, nums):
+        self.n = len(nums)
+        self.bit = [0] * (self.n+1)
+        for i in range(1, self.n + 1):
+            self.point_update_query(i, nums[i - 1])
+
+    def point_update_query(self, index, diff):
+        while index < len(self.bit):
+            self.bit[index] += diff
+            index = self.get_next_index(index)
+
+    def range_sum_query_util(self, index):
         index += 1
-        while index <= self.n:
-            self.fenwick_tree[index] += val
-            index += (index & (-index))
-
-    def query(self, index):
-        res = 0
-        index += 1
+        ans = 0
         while index > 0:
-            res += self.fenwick_tree[index]
-            index -= (index & (-index))
-        return res
+            ans += self.bit[index]
+            index = self.get_parent_index(index)
+        return ans
 
-    def range(self, left, right):
-        return self.query(right) - self.query(left - 1)
+    def range_sum_query(self, left: int, right: int):
+        left = self.range_sum_query_util(left-1) if left > 0 else 0
+        right = self.range_sum_query_util(right)
+        return right - left
 
+    
+bit = BinaryIndexedTree([1,2,3,4,5,6,7,8,9,10])
+print(bit.range_sum_query_util(9))
+print(bit.range_sum_query_util(8))
+print(bit.range_sum_query_util(7))
+print(bit.range_sum_query_util(6))
+print(bit.range_sum_query_util(5))
+print(bit.range_sum_query_util(4))
+print(bit.range_sum_query_util(3))
+print(bit.range_sum_query_util(2))
+print(bit.range_sum_query_util(1))
+print(bit.range_sum_query_util(0))
 
-ft = FenwickTree(10, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-print(ft.fenwick_tree)
-print(ft.range(0, 9))
-print(ft.range(3, 3))
-print(ft.range(4, 7))
-print(ft.range(8, 9))
-print(ft.range(0, 1))
-print(ft.range(0, 0))
-print(ft.range(9, 9))
+print(bit.range_sum_query(3, 9))
+print(bit.range_sum_query(5, 6))
+print(bit.range_sum_query(0, 6))
+print(bit.range_sum_query(3, 3))
