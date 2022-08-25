3857b1b5444ba623fa4f948b561b645097ad71c9
anCoderr <deyol.anurag999@gmail.com>
2022-08-25 20:21:28 +0530

    java started

---
+++ DataStructures_and_Algorithms_in_JAVA/Runner.java
@@ -0,0 +1,11 @@
+
+
+public class Runner {
+    public static void main(String[] args) throws Exception {
+        System.out.println("HELLO WORLD!!");
+        int n = 10;
+        for(int i = 0; i<n; i++) {
+            System.out.println(i + " time tested");
+        }
+    }
+}
---
+++ Programming_in_Python/test.py
@@ -0,0 +1,13 @@
+import sortedcontainers as ss
+
+mylist = ss.SortedList()
+
+print(mylist)
+mylist.add(10)
+mylist.add(9)
+mylist.add(4)
+mylist.add(5)
+mylist.add(1)
+mylist.add(3)
+mylist.add(7)
+print(mylist)
